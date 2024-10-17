7eca22b48b58af55deae3fcf22030cfda4c9b63c
Joel Akrowey Kaku <131392907+joelkaku@users.noreply.github.com>
2024-10-17 12:41:31 +0000

    Update comb.py

--- comb.py
+++ comb.py
@@ -18,7 +18,6 @@ if not st.session_state['logged_in']:
     st.text_input("Username", value="Admin", disabled=True)  # Make username uneditable
 
     password = st.text_input("Password", type="password")
-    show_password = st.checkbox("Show password")
 
     if show_password:
         st.text_input("Password", value=password)
@@ -49,38 +48,38 @@ else:
 
     with col1:
         # Age input
-        age = st.number_input("Age (18 and above)", min_value=18)
+        age = st.number_input("Age (18 and above)", min_value=18, value=0)
 
     with col2:
         # Annual income input
-        annual_income = st.number_input("Annual Income (Dollars)", min_value=0.0, step=1000.0)
+        annual_income = st.number_input("Annual Income (Dollars)", min_value=0.0, step=1000.0, value=0)
 
     with col3:
         # Home ownership dropdown
-        home_ownership = st.selectbox("Home Ownership Status", ("Own", "Mortgage", "Rent"))
+        home_ownership = st.selectbox("Home Ownership Status", ("Own", "Mortgage", "Rent"), value=0)
 
     # Create another row with 3 columns for Employment Duration, Loan Purpose, and Loan Amount
     col4, col5, col6 = st.columns(3)
 
     with col4:
         # Employment Duration
-        employment_duration = st.number_input("Employment Duration (Years)", min_value=0)
+        employment_duration = st.number_input("Employment Duration (Years)", min_value=0, value=0)
 
     with col5:
         # Purpose of the loan dropdown
-        loan_purpose = st.selectbox("Purpose of the Loan", ("Education", "Home Improvement"))
+        loan_purpose = st.selectbox("Purpose of the Loan", ("Education", "Home Improvement"), value=0)
 
     with col6:
         # Loan applied
-        loan_applied = st.number_input("Loan Applied (Dollars)", min_value=0.0, step=500.0)
+        loan_applied = st.number_input("Loan Applied (Dollars)", min_value=0.0, step=500.0, value=0)
     
     col7, col8, col9 = st.columns(3)
     with col7:
-        rate = st.number_input("Rate (%)", min_value=0.0, step=0.1)
+        rate = st.number_input("Rate (%)", min_value=0.0, step=0.1, value=0)
     with col8:
-        default = st.selectbox("Default",("Yes","No"))
+        default = st.selectbox("Default",("Yes","No"), value=0)
     with col9:
-        credit_history = st.number_input("Credit History", min_value =0.0, step=1.0)
+        credit_history = st.number_input("Credit History", min_value =0.0, step=1.0, value=0)
 
 
 
